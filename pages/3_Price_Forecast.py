0c932079b463e1e8217330bd75525bb5da95167f
Praew <warumpa@bu.edu>
2025-04-25 15:46:18 -0400

    change dash

--- pages/3_Price_Forecast.py
+++ pages/3_Price_Forecast.py
@@ -54,7 +54,7 @@ try:
     if model_price:
         st.markdown(f"🧠 Model-Based Price Estimate: **${model_price:.2f}**")
     if peer_price_min and peer_price_max:
-        st.write("🏢 Peer-Based Price Range Estimate:", f"**${peer_price_min:.2f} - ${peer_price_max:.2f}**")
+        st.markdown(f"📊 Peer-Based Price Range Estimate: **${peer_price_min:.2f} - ${peer_price_max:.2f}**")
     if analyst_price:
         st.markdown(f"📣 Analyst Price Estimate: **${analyst_price:.2f}**")
 
